2787d4bd3657335fe158bb51a37a2aa7f76c4c51
kylewellband <kyle.wellband@gmail.com>
2019-11-07 09:18:16 -0500

    Remove ploidy in pool continous mode

--- 01_scripts/06_freebayes.sh
+++ 01_scripts/06_freebayes.sh
@@ -10,14 +10,13 @@ LOG_FOLDER="10_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 # Global variables
-VCFLIB="/home/kylewellband/.bin/freebayes/vcflib" # Path to vcflib
 GENOME="02_reference/genome.fasta"  # Genomic reference.fasta
 INPUT="07_clipped_overlap"
 OUTPUT="08_raw_vcfs"
 TMP="99_tmp"
-NCPUS=4
+NCPUS=14
 PLOIDY=80 # this should be N individuals x 2 for diploid organisms
-POOL_MODEL="-F 0.01 -C 2 --pooled-continuous" # alternative to give similar results to GATK: "--pooled-discrete --use-best-n-alleles 3"
+POOL_MODEL="-F 0.01 -C 2 --pooled-continuous" # alternative to give similar results to GATK: "--pooled-discrete -p $PLOIDY --use-best-n-alleles 3"
 MAX_COV=100 # maximum depth, downsample sites above this depth
 
 # Modules
@@ -32,15 +31,19 @@ do
     
     echo "Calling SNPs in: $file"
     
-	 freebayes \
-        -f $GENOME \
-        -p $PLOIDY \
-        --min-coverage 10 --use-best-n-alleles 3 --min-alternate-count 2 \
-        $POOL_MODEL \
-        --limit-coverage $MAX_COV \
-        "$file".bam > $OUTPUT/"$name"_freebayes.vcf.gz
+	01_scripts/util/fasta_generate_regions.py "$GENOME".fai 1000000 | 
+    	parallel --progress --joblog $LOG_FOLDER/"$TIMESTAMP"_freebayes.log -k -j $NCPUS \
+        freebayes \
+            -f $GENOME \
+            --region {} \
+            --min-coverage 10 --use-best-n-alleles 3 --min-alternate-count 2 \
+            $POOL_MODEL \
+            --limit-coverage $MAX_COV \
+            "$file".bam | 
+    	vcffirstheader |
+    	vcfstreamsort -w 1000 | 
+    	vcfuniq | gzip > $OUTPUT/"$name"_freebayes.vcf.gz
 
 done
 
-#cat $TMP/"$name"_*.vcf.gz | vcffirstheader | vcfstreamsort -w 1000 | 
-#    vcfuniq | gzip $OUTPUT/"$name"_freebayes.vcf.gz
+
